352648ff8b821cd40eb40978353b9cbe0b358f64
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-06-09 16:30:28 +0200

    set up

---
+++ elweather/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          El Weather
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
